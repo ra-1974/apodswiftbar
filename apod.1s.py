659a0dc213c2a4a876996926418a5a148c1acd47
= <jake@vossen.dev>
2019-05-07 11:25:08 -0600

    Image resizing working

--- apod.1s.py
+++ apod.1s.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
 import base64
 import urllib.request
+from PIL import Image
+import glob, os
+import io
+from io import BytesIO
 
 # with open("/tmp/temp.jpg", "rb") as image_file:
     # encoded_string = str(base64.b64encode(image_file.read()))[2:][:-1]
@@ -25,12 +29,23 @@ image_url = "https://apod.nasa.gov/apod/" + image_url
 
 image_req = urllib.request.Request(image_url)
 
-image_file = urllib.request.urlopen(image_req).read()
-encoded_string = base64.b64encode(image_file)
+image_path = io.BytesIO(urllib.request.urlopen(image_req).read())
+pillow_image = Image.open(image_path)
+# pillow_image.show()
+maxsize = (720, 720)
 
+pillow_image.thumbnail(maxsize)
+buff = BytesIO()
+pillow_image.save(buff, format="JPEG")
+encoded_string = base64.b64encode(buff.getvalue())
+# base64_data = base64.b64encode(binary_data)
+
+
+
+# encoded_string = ""
 # except:
     # print("There was an error, most likely with SSL certificates. Click me to fix |href=https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org")
     
 # print(r)
 # image_string = "image=" + encoded_string
-print("| href=# image=" +  str(encoded_string)[2:][:-1])
+print("| href=https://apod.nasa.gov/apod/ image=" +  str(encoded_string)[2:][:-1])
